62d900d8ab14326a00be024130544fbed68c867d
Guillaume Lethuillier <guillaume.lethuillier@ledger.fr>
2021-01-28 17:29:20 +0100

    refactor(setup_live.py): clean code

--- setup_live.py
+++ setup_live.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 import yaml
-import sys 
+import sys, getopt
 import os
 import platform
 import shutil
@@ -9,22 +9,56 @@ import fileinput
 from pathlib import Path
 import subprocess
 from termcolor import colored
+from enum import Enum
+
+class OS(Enum):
+    WINDOWS = 0
+    MACOS = 1
+    LINUX = 2
+
+operating_system: OS = None
 
 node_major_version = 12
-verbose_mode = True
+verbose_mode = False
 
 emulator = 'Pixel_XL_API_30'
 
 script_dir = os.path.dirname(os.path.realpath(__file__))
 workdir_path = os.path.join(script_dir, '.live_sources')
 
-operating_system = platform.system()
+# args
+try:
+    opts, args = getopt.getopt(sys.argv[1:],"hc:m",["coin=","mobile="])
+except getopt.GetoptError:
+    print ('setup_live.py -c <coin name> [-m <ios|android>]')
+    sys.exit(1)
+
+mobile = None
+coin = None
+for opt, arg in opts:
+    if opt in ("-c", "--coin"):
+        coin = arg
+        print ('coin: ', coin)
+    elif opt in ("-m", "--mobile"):
+        mobile = arg
+
+if coin is None:
+    print('Coin is required: setup_live.py -c <coin name>')
+    sys.exit(1)
+
+# operating system
+if platform.system() == "Windows":
+    operating_system = OS.WINDOWS
+elif platform.system() == "Darwin":
+    operating_system = OS.MACOS
+else:
+    operating_system = OS.LINUX
 
 class EmptyVariable(Exception):
     pass
 
 # TODO: check that what is returned is not empty
-def get_product_configuration(coin, product):
+def get_product_configuration(coin):
     with open(os.path.join(script_dir, 'config.yml'), 'r') as stream:
         try:
             sources = yaml.safe_load(stream)
@@ -35,30 +69,22 @@ def get_product_configuration(coin, product):
             sys.stderr.write(f"Error: variable has not been set for {coin} ([{product}])\n")
             exit(1)
 
-    return sources[coin][product]
+    return sources[coin]['live']
 
-
-coin = sys.argv[1].lower()
-config = get_product_configuration(coin, 'live')
-
-if len(sys.argv) < 3:
-    mobile = None
-else:
-    mobile = sys.argv[2].lower()
+config = get_product_configuration(coin)
 
 """
 Run a system command
 """
-
 def run(command, cwd=None, verbose=False, silent=False):
     if cwd is None:
         cwd = Path.home()
 
     shell = False
-    if operating_system == "Windows":
+    if operating_system == OS.WINDOWS:
         shell = True
 
-    if verbose == True:
+    if verbose:
         command += ' --verbose'
 
     out = []
@@ -86,6 +112,7 @@ def run(command, cwd=None, verbose=False, silent=False):
         _, _ = p.communicate()
   
     return '\n'.join(out), '\n'.join(err)
+    
 
 def check_tooling():
     tools = [
@@ -108,10 +135,10 @@ def check_tooling():
             sys.exit(1)
 
 def check_node_version(node_version):
-    if operating_system == 'Windows':
+    if operating_system == OS.WINDOWS:
         return
 
-    out, _ = run('node --version', quiet=True)
+    out, _ = run('node --version', silent=True)
     if f"v{node_version}" not in out:
         print(f"Error: node version should be v{node_version}")
         print(f"Current version: {out}")
@@ -120,7 +147,7 @@ def check_node_version(node_version):
 def remove_dir(dir_path):
     print(colored('Deleting ' + dir_path, 'yellow'), end='')
     # Prevent permission denied error
-    if operating_system == "Windows":
+    if operating_system == OS.WINDOWS:
         os.system('rmdir /q /s "{}" 2>NUL'.format(dir_path))
     else:
         shutil.rmtree(dir_path, ignore_errors=True)
@@ -139,18 +166,20 @@ def create_workdir():
 def clear_cache():
     home = str(Path.home())
 
-    if operating_system == 'Darwin':
+    if operating_system in (OS.LINUX, OS.MACOS):
         cache_folders = [
             '/.yalc', 
             '/.yarn', 
             '/.npm'
             ]
-    elif operating_system == 'Windows':
+    elif operating_system == OS.WINDOWS:
         cache_folders = [
             r'\AppData\Local\Yalc', 
             r'\AppData\Local\Yarn\cache', 
             r'\AppData\Roaming\npm-cache'
             ]
+    else:
+        return
 
     data = input("Do you want to clear the cache?\n(" + ', '.join(cache_folders) + ' will be cleared)\nyN ')
     if data.lower() != 'y':
@@ -179,7 +208,7 @@ def clone(app, path):
 Fix package.json to make it compatible with Windows
 """
 def fix_package(path):
-    if operating_system == "Windows": 
+    if operating_system == OS.WINDOWS:
         package_file = os.path.join(path, 'package.json')
         for line in fileinput.input(package_file, inplace=True):
             print(line.replace("./scripts/", "bash ./scripts/"), end='')
@@ -190,69 +219,69 @@ prepare()
 # Live-common
 print(colored(' LIVE-COMMON ', 'blue', 'on_yellow'))
 
-live_common_workdir_path = os.path.join(workdir_path, 'ledger-live-common')
+lc_workdir = os.path.join(workdir_path, 'ledger-live-common')
 
-clone('live_common', live_common_workdir_path)
+clone('live_common', lc_workdir)
 
-fix_package(live_common_workdir_path)
+fix_package(lc_workdir)
 
-run('yarn install', live_common_workdir_path, verbose=verbose_mode)
-run('yalc publish --push', live_common_workdir_path)
+run('yarn install', cwd=lc_workdir, verbose=verbose_mode)
+run('yalc publish --push', cwd=lc_workdir)
 
 # # # Live-common: CLI
 print(colored(' LIVE-COMMON: CLI ', 'blue', 'on_yellow'))
 
-live_common_CLI_workdir_path = os.path.join(workdir_path, 'ledger-live-common', 'cli')
+cli_workdir = os.path.join(workdir_path, 'ledger-live-common', 'cli')
 
-run('yalc add @ledgerhq/live-common', live_common_CLI_workdir_path)
+run('yalc add @ledgerhq/live-common', cwd=cli_workdir)
 
-fix_package(live_common_CLI_workdir_path)
+fix_package(cli_workdir)
 
-run('yarn install', live_common_CLI_workdir_path, verbose=verbose_mode)
-run('yarn build', live_common_CLI_workdir_path, verbose=verbose_mode)
+run('yarn install', cwd=cli_workdir, verbose=verbose_mode)
+run('yarn build', cwd=cli_workdir, verbose=verbose_mode)
 
 # Live Desktop
 print(colored(' DESKTOP ', 'blue', 'on_yellow'))
 
-live_desktop_workdir_path = os.path.join(workdir_path, 'ledger-live-desktop')
+desktop_workdir = os.path.join(workdir_path, 'ledger-live-desktop')
 
-clone('live_desktop', live_desktop_workdir_path)
+clone('live_desktop', desktop_workdir)
 
-run('yalc add @ledgerhq/live-common', live_desktop_workdir_path)
-run('yarn install', live_desktop_workdir_path, verbose=verbose_mode)
-run("yarn-deduplicate", live_desktop_workdir_path, verbose=verbose_mode)
-run('yarn install', live_desktop_workdir_path, verbose=verbose_mode)
+run('yalc add @ledgerhq/live-common', cwd=desktop_workdir)
+run('yarn install', cwd=desktop_workdir, verbose=verbose_mode)
+run("yarn-deduplicate", cwd=desktop_workdir, verbose=verbose_mode)
+run('yarn install', cwd=desktop_workdir, verbose=verbose_mode)
 
 # Mobile
 if mobile is not None:
-    live_mobile_workdir_path = os.path.join(workdir_path, 'ledger-live-mobile')
+    mobile_workdir = os.path.join(workdir_path, 'ledger-live-mobile')
 
     print(colored(' MOBILE ', 'blue', 'on_yellow'))
 
-    clone('live_mobile', live_mobile_workdir_path)
+    clone('live_mobile', mobile_workdir)
 
-    run('yalc add @ledgerhq/live-common', live_mobile_workdir_path)
-    run('bundle install', live_mobile_workdir_path) 
-    run('yarn install', live_mobile_workdir_path)
+    run('yalc add @ledgerhq/live-common', cwd=mobile_workdir)
+    run('bundle install', cwd=mobile_workdir) 
+    run('yarn install', cwd=mobile_workdir)
 
     for i in range(0, 3):
-        run('yarn pod', live_mobile_workdir_path)
+        run('yarn pod', mobile_workdir)
 
-    run('yarn', live_mobile_workdir_path)
-    run('yarn start &', live_mobile_workdir_path)
+    run('yarn', cwd=mobile_workdir)
+    run('yarn start &', cwd=mobile_workdir)
 
     if mobile == 'android':
         print(colored('Run Android', 'blue'))
-        run('adb start-server', live_mobile_workdir_path)
-        run(f"emulator -avd ${emulator}", live_mobile_workdir_path)
+        run('adb start-server', cwd=mobile_workdir)
+        run(f"emulator -avd ${emulator}", cwd=mobile_workdir)
 
-        run('yarn run android', live_mobile_workdir_path)
+        run('yarn run android', cwd=mobile_workdir)
 
     elif mobile == 'ios':
         print(colored('Run iOS', 'blue'))
-        run('yarn run ios', live_mobile_workdir_path)
+        run('yarn run ios', cwd=mobile_workdir)
 
 
 # Display info
-print("node {}/bin/index.js".format(live_common_CLI_workdir_path))
-print("cd {} && yarn start".format(live_desktop_workdir_path))
+print("node {}/bin/index.js".format(cli_workdir))
+print("cd {} && yarn start".format(desktop_workdir))
